1fe353f03182a642a3e26574076ae0100c36f488
mohamedShawki <[muhammad.shawkii@gmail.com]>
2025-12-21 20:00:09 +0200

    Update Landing Page version2

---
+++ src/app/Components/home/Components/our-partners/our-partners.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-our-partners',
+  imports: [],
+  templateUrl: './our-partners.component.html',
+  styleUrl: './our-partners.component.css'
+})
+export class OurPartnersComponent {
+
+}
