ff6132c940564da6f51a22250d2d72991c94e180
mohamedShawki <[muhammad.shawkii@gmail.com]>
2026-01-10 19:17:36 +0200

    front fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,9 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'Al-Maaly-Gate-School';
+
+  constructor(private translate: TranslateService) {
+    this.translate.setDefaultLang('en');
+    this.translate.use('ar');
+  }
 }
