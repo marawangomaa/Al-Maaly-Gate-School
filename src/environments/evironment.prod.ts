4c82f97bb8ea7507b030aafffa943f6f66951f81
mohamedShawki <[muhammad.shawkii@gmail.com]>
2026-01-16 16:13:27 +0200

    envirnment fixes

--- src/environments/evironment.prod.ts
+++ src/environments/evironment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: true,
     apiUrl: 'http://72.62.178.68/api',
     baseUrl: 'http://72.62.178.68',
-    imageBaseUrl: 'http://72.62.178.68/api'
+    imageBaseUrl: 'http://72.62.178.68'
 };
